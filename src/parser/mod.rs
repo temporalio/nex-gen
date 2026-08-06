2eb6b98e921f080563300689e481e87ac6db1146
Alex Mazzeo <alex.mazzeo@temporal.io>
2026-08-06 13:21:27 -0700

    separate type rendering from parsing

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -18,8 +18,8 @@ pub(crate) use json_schema::{ManifestModel, ManifestService, NameManifest, build
 pub use wit::{load_api_spec_from_wit_for_language_with_inputs, write_prepared_wit_directory};
 
 pub(crate) use wit::{
-    LinkedWitMetadata, find_proto_name_for_type, find_proto_name_for_type_def,
-    load_linked_wit_metadata_from_inputs, parse_wit_with_inputs, render_wit_type, select_world,
+    LinkedWitMetadata, ParsedWitType, find_proto_name_for_type, find_proto_name_for_type_def,
+    load_linked_wit_metadata_from_inputs, parse_wit_type, parse_wit_with_inputs, select_world,
     wire_operation_name_from_docs,
 };
 
